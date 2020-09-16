48be1cc4600097420cbb30d15c1a167a6c7220a9
Kah919 <kahyap13@gmail.com>
2020-09-16 18:42:29 -0400

    Test equality and greater than

--- functions.test.js
+++ functions.test.js
@@ -15,3 +15,25 @@ test('Should be null', () => {
 test('Should be falsy', () => {
     expect(functions.checkValue(null)).toBeFalsy()
 })
+
+// toBe is for primitive and toEqual is for objects, arrays
+test('Expect user to be Kah', () => {
+    expect(functions.createUser()).toEqual({
+        firstName: 'Kah',
+        lastName: 'Yap'
+    })
+})
+
+test('Should be under 1600', () => {
+    const load1 = 800;
+    const load2 = 700;
+    expect(load1 + load2).toBeLessThan(1600);
+});
+
+// toBeLessThan
+// toBeLessThanOrEqual
+
+// Regex
+test('There is no I in team', () => {
+    expect('team').not.toMatch(/I/)
+})
