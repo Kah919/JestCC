3dca8b8c6ea682545936e76745c70f029c4ab519
Kah919 <kahyap13@gmail.com>
2020-09-17 11:56:43 -0400

    Promise and Async Await Test

--- functions.js
+++ functions.js
@@ -1,3 +1,5 @@
+const { default: Axios } = require("axios");
+
 const functions = {
     add: (num1, num2) => num1 + num2,
     isNull: () => null,
@@ -6,7 +8,10 @@ const functions = {
         const user = { firstName: 'Kah' }
         user['lastName'] = 'Yap'
         return user
-    }
+    },
+    fetchUser: () => Axios.get('https://jsonplaceholder.typicode.com/users/1')
+        .then(res => res.data)
+        .catch(err => 'error')
 }
 
 module.exports = functions;
